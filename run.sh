d58b80e21af0550dc7640d460b5721b55ffef111
dff1980 <pp.zhukov@gmail.com>
2017-10-16 15:19:59 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 kubectl create -f PersistentVolume.yaml
+kubectl get pv
 kubectl create secret generic mysql-pass --from-literal=password=n0v3ll
+kubectl create -f mysql-deployment.yaml
+kubectl get pods
