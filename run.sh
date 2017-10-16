e4fa97f12f8159b52d6459c79d04c0137458d03d
dff1980 <pp.zhukov@gmail.com>
2017-10-16 17:09:08 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 # Create two PersistentVolumes
-kubectl create -f PersistentVolume.yaml
+kubectl create -f persistentvolume.yaml
 #verify that two 20GiB PersistentVolumes are available
 kubectl get pv
 #Create the Secret object for MySQL Password
 kubectl create secret generic mysql-pass --from-literal=password=n0v3ll
 #Deploy MySQL
-kubectl create -f MySQL-deployment.yaml
+kubectl create -f mysql-deployment.yaml
 #Verify that the Pod is running
 kubectl get pods
 #Create a WordPress Service and Deployment
-kubectl create -f Wordpress-deployment.yaml
+kubectl create -f wordpress-deployment.yaml
 #Verify that the Service is running
 kubectl get services wordpress
