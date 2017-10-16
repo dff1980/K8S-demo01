1cbd0d2c025316720226162400a90d406415e759
dff1980 <pp.zhukov@gmail.com>
2017-10-16 15:15:12 +0300

    Create clean_up.sh

---
+++ clean_up.sh
@@ -0,0 +1,8 @@
+#delete your Secret
+kubectl delete secret mysql-pass
+#delete all Deployments and Services:
+kubectl delete deployment -l app=wordpress
+kubectl delete service -l app=wordpress
+#delete the PersistentVolumeClaims and the PersistentVolumes:
+kubectl delete pvc -l app=wordpress
+kubectl delete pv local-pv-1 local-pv-2
