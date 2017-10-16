362aa103f528976285a865474d62d269dfcc097f
dff1980 <pp.zhukov@gmail.com>
2017-10-16 15:16:05 +0300

    Update clean_up.sh

--- clean_up.sh
+++ clean_up.sh
@@ -5,4 +5,4 @@ kubectl delete deployment -l app=wordpress
 kubectl delete service -l app=wordpress
 #delete the PersistentVolumeClaims and the PersistentVolumes:
 kubectl delete pvc -l app=wordpress
-kubectl delete pv local-pv-1 local-pv-2
+kubectl delete pv mysql-pv wordpress-pv
