e3e3e44c333cfa2564c99977eb08b7acf6517c70
root <root@router.caasp.novell-cis.ru>
2017-11-28 13:08:15 +0300

    new file:   dashboard/grafana-service.yaml 	modified:   dashboard/kubernetes-dashboard.sh 	modified:   dashboard/kubernetes-dashboard.yaml 	new file:   dashboard/remove-kubernetes-dashboard.sh 	renamed:    clean_up.sh -> wordpress/clean_up.sh 	renamed:    mysql-deployment.yaml -> wordpress/mysql-deployment.yaml 	renamed:    persistentvolume.yaml -> wordpress/persistentvolume.yaml 	renamed:    run.sh -> wordpress/run.sh 	renamed:    wordpress-deployment.yaml -> wordpress/wordpress-deployment.yaml

--- dashboard/kubernetes-dashboard.sh
+++ dashboard/kubernetes-dashboard.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-#kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.6.3/src/deploy/kubernetes-dashboard.yaml
-#kubectl apply -f https://raw.githubusercontent.com/SUSE/caasp-services/master/contrib/
-#addons/heapster/heapster.yaml
-#kubectl apply -f https://raw.githubusercontent.com/SUSE/caasp-services/master/contrib/
-#addons/heapster/heapster.yaml
-#kubectl apply -f https://raw.githubusercontent.com/kubernetes/heapster/master/deploy/
-#kube-config/influxdb/influxdb.yaml
-#kubectl apply -f https://raw.githubusercontent.com/kubernetes/heapster/release-1.3/
-#deploy/kube-config/influxdb/grafana-deployment.yaml
-#wget https://raw.githubusercontent.com/kubernetes/heapster/release-1.3/deploy/kubeconfig/influxdb/grafana-service.yaml
 
+# Sources:
+# https://raw.githubusercontent.com/kubernetes/dashboard/v1.6.3/src/deploy/kubernetes-dashboard.yaml
+# https://raw.githubusercontent.com/SUSE/caasp-services/master/contrib/addons/heapster/heapster.yaml
+# https://raw.githubusercontent.com/kubernetes/heapster/master/deploy/kube-config/influxdb/influxdb.yaml
+# https://raw.githubusercontent.com/kubernetes/heapster/release-1.3/deploy/kube-config/influxdb/grafana-deployment.yaml
+# https://raw.githubusercontent.com/kubernetes/heapster/release-1.3/deploy/kube-config/influxdb/grafana-service.yaml
 
-wget https://raw.githubusercontent.com/kubernetes/dashboard/v1.6.3/src/deploy/kubernetes-dashboard.yaml
-wget https://raw.githubusercontent.com/SUSE/caasp-services/master/contrib/addons/heapster/heapster.yaml
-wget https://raw.githubusercontent.com/SUSE/caasp-services/master/contrib/addons/heapster/heapster.yaml
-wget https://raw.githubusercontent.com/kubernetes/heapster/master/deploy/kube-config/influxdb/influxdb.yaml
-wget https://raw.githubusercontent.com/kubernetes/heapster/release-1.3/deploy/kube-config/influxdb/grafana-deployment.yaml
-wget https://raw.githubusercontent.com/kubernetes/heapster/release-1.3/deploy/kubeconfig/influxdb/grafana-service.yaml
+kubectl apply -f heapster.yaml
+kubectl apply -f influxdb.yaml
+kubectl apply -f grafana-deployment.yaml
+kubectl apply -f grafana-service.yaml
+kubectl create -f kubernetes-dashboard.yaml
