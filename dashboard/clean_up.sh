c0ad8e7483db02681a4497ffe52205776780647d
root <root@router.caasp.novell-cis.ru>
2017-11-28 16:01:45 +0300

    modified:   dashboard/clean_up.sh

--- dashboard/clean_up.sh
+++ dashboard/clean_up.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+
+
 for name in kubernetes-dashboard monitoring-grafana monitoring-influxdb heapster
  do
   kubectl -n kube-system delete service $name
   kubectl -n kube-system delete deployment $name
  done
 
-kubectl -n kube-system delete ClusterRole system:heapster
+kubectl -n kube-system delete serviceaccounts kubernetes-dashboard
+kubectl -n kube-system delete serviceaccounts heapster
+kubectl -n kube-system delete ClusterRoleBinding heapster
+kubectl -n kube-system delete ClusterRoleBinding kubernetes-dashboard
+
